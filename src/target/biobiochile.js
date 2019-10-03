4262b734004623fde5a4bb4adec27695bf04b13b
swon3210 <swon3210@gmail.com>
2019-10-03 17:42:59 +0900

    error handling about missing data

--- src/target/biobiochile.js
+++ src/target/biobiochile.js
@@ -49,23 +49,43 @@ const biobiochile = {
 
         // $eval 은 브라우저 컨텍스트 $$은 퍼펫티어 컨텍스트
         
-        const news_thumnail_blocks = await page.$$('#container1 > .content .small-content > .noticia');
+        const news_thumnail_blocks = await page.$$('#container1 > .content .small-content > .noticia').catch(err => {
+          console.log('cannot reach to thumnail contents!')
+          console.log(err);
+          return null;
+        });
 
         for (thumnail_block of news_thumnail_blocks) {
 
           // 아 맵으로 해야되는구나 $$나 $$eval은
 
-          const [thumnail_date, thumnail_time] = await thumnail_block.$$eval('div[class="col-xs-2 fecha"] p', elements => elements.map(e => e.innerText));
+          const [thumnail_date, thumnail_time] = await thumnail_block.$$eval('div[class="col-xs-2 fecha"] p', elements => elements.map(e => e.innerText)).catch(err => {
+            console.log('thumnail_date and thumnail_time are missing!')
+            console.log(err);
+            return null;
+          });
 
-          const thumnail_author = await thumnail_block.$eval('p[class="autor"] > a', element => element.innerText);
+          const thumnail_author = await thumnail_block.$eval('p[class="autor"] > a', element => element.innerText).catch(err => {
+            console.log('thumnail_author is missing!')
+            console.log(err);
+            return null;
+          });
 
           const [ thumnail_link, thumnail_title ] = await thumnail_block.$eval('div[class="col-xs-6 titular-fecha"] a', element => {
             return [ element.href, element.innerText ];
-          })
+          }).catch(err => {
+            console.log('thumnail_link and thumnail_title are missing!')
+            console.log(err);
+            return null;
+          });
 
           const thumnail_image_link = await thumnail_block.$eval('div[class="col-xs-4 contenedor-imagen"] div', element => {
             return element.style['background-image'].replace('url("//', "").replace('")', "");    
-          })
+          }).catch(err => {
+            console.log('thumnail_image_link is missing!')
+            console.log(err);
+            return null;
+          });
 
           const thumnail_obj = {
             thumnail_date,
@@ -75,7 +95,6 @@ const biobiochile = {
             thumnail_title,
             thumnail_image_link
           }
-
           biobiochile.thumnail_lists[category].push(thumnail_obj);
         }
 
@@ -84,6 +103,10 @@ const biobiochile = {
         const pagination = await page.$eval('#container1 > .content .small-content nav[aria-label="Page navigation example"] li[class="page-item active"]', element => {
           element.nextElementSibling.children[0].click();
           return element.innerText;
+        }).catch(err => {
+          console.log('pagination is missing!');
+          console.log(err);
+          return null;
         });
 
         console.log('pagination : ', limit);
@@ -105,15 +128,53 @@ const biobiochile = {
     for (let thumnail of biobiochile.thumnail_lists[category]) {
       await page.goto(thumnail.thumnail_link);
 
-      const post_data = await page.$('#content-general-nota div[class^="caja-blanca"]');
+      const post_data = await page.$('#content-general-nota div[class^="caja-blanca"]').catch(err => {
+        console.log('cannot reach to the post contents');
+        console.log(err);
+        return null;
+      });
 
-      const post_region = await post_data.$eval('div[class="categoria-titulo-nota"]', element => element.innerText);
-      const post_date = await post_data.$eval('div[class="nota-fecha am-hide"]', element => element.innerText);
-      const post_title = await post_data.$eval('div[class="nota-titular robotos"]', element => element.innerText);
-      const post_image_link = await post_data.$eval('div[class="nota-container"] > div[class="nota-body"] > div[class="nota-img nota-img-chica"] img', element => element.src);
+      const post_region = await post_data.$eval('div[class="categoria-titulo-nota"]', element => element.innerText).catch(err => {
+        console.log('post region is missing!');
+        console.log(err);
+        return null;
+      });
+      const post_date = await post_data.$eval('div[class="nota-fecha am-hide"]', element => element.innerText).catch(err => {
+        console.log('post date is missing!');
+        console.log(err);
+        return null;
+      });
+      const post_title = await post_data.$eval('div[class="nota-titular robotos"]', element => element.innerText).catch(err => {
+        console.log('post title is missing!')
+        console.log(err);
+        return null;
+      });
 
-      const [post_author, post_provider ] = await post_data.$eval('div[class="nota-autor"]', element => element.innerText.split('\n'));
-      const post_content = await post_data.$eval('div[class="nota-container"] > div[class="nota-body"] > div[class="nota-contenido-fondo"] > div[class^="nota-contenido"]', element => element.innerText);
+      const post_image_link = await post_data.$eval('div[class="nota-container"] > div[class="nota-body"] > div[class="nota-img nota-img-chica"] img', element => element.src).catch(err => {
+        console.log('image link is missing!')
+        console.log(err);
+        return null;
+      });
+      
+      const post_video_link = await post_data.$eval('div[class="nota-container"] > div[class="nota-body"] iframe[id="vrudo-destacado"]', element => {
+        return element.contentWindow.document.body.querySelector('video > source').src;
+      }).catch(err => {
+        console.log('video is wrong!')
+        console.log(err);
+        return null;
+      });
+      
+
+      const [post_author, post_provider ] = await post_data.$eval('div[class="nota-autor"]', element => element.innerText.split('\n')).catch(err => {
+        console.log('post_author and post_provider are missing!')
+        console.log(err);
+        return null;
+      });
+      const post_content = await post_data.$eval('div[class="nota-container"] > div[class="nota-body"] > div[class="nota-contenido-fondo"] > div[class^="nota-contenido"]', element => element.innerText).catch(err => {
+        console.log('content is missing!')
+        console.log(err);
+        return null;
+      });
 
       const post_obj = {
         post_region,
